20271d6e3057e364ff1f5128361210c118f60118
ChrisMagnuson <hourglasssand@gmail.com>
2017-10-20 08:21:54 -0400

    Add Find-VMSNeedingVMVersionUpgrade and Update-VMSNeedingVMVersionUpgrade

--- TervisHyper-V.psm1
+++ TervisHyper-V.psm1
@@ -1,1 +1,15 @@
-﻿
+function Find-VMSNeedingVMVersionUpgrade {
+    $nodes = Get-ClusterNode -Cluster hypervcluster5 | select -ExpandProperty Name 
+    foreach ($node in $nodes) {
+        $updated += Get-VM -ComputerName $Node | where version -ne '8.0' | where state -eq running
+    }
+    $updated | Sort Name
+    $updated | measure | Select Count 
+}
+
+function Update-VMSNeedingVMVersionUpgrade {
+    $nodes = Get-ClusterNode -Cluster hypervcluster5 | select -ExpandProperty Name
+    foreach ($node in $nodes) {
+        Get-VM -ComputerName $Node | Where State -eq Off | where version -ne '8.0' | Update-VMVersion -Confirm:$false
+    }
+}
